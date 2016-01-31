78af9ecbaddc7e77eacb4de45836276e33c2bade
sanketmaru <sanketmaru@gmail.com>
2016-02-01 00:08:47 +0530

    modify to start server

--- server.js
+++ server.js
@@ -1,38 +1,25 @@
-// including libraries
-var http = require('http');
-var static = require('node-static');
-var app = http.createServer(handler);
-var io = require('socket.io').listen(app);
+var app = require('http').createServer(handler)
+var io = require('socket.io')(app);
+var fs = require('fs');
 
-// define port
-var port = 9000;
+app.listen(9000);
 
-// make html, js & css files accessible
-var files = new static.Server('./public');
+function handler (req, res) {
+  fs.readFile('./public' + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
 
-// serve files on request
-function handler(request, response) {
-	request.addListener('end', function() {
-		files.serve(request, response);
-	});
+    res.writeHead(200);
+    res.end(data);
+  });
 }
 
-// app.use('/', function(req, res) {
-//     res.render('index');
-// });
-
-
-// listen for incoming connections from client
-io.sockets.on('connection', function (socket) {
-
-  // start listening for coords
-  socket.on('send:coords', function (data) {
-
-  	// broadcast your coordinates to everyone except you
-  	socket.broadcast.emit('load:coords', data);
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
   });
 });
-
-// starts app on specified port
-app.listen(port);
-console.log('Your server goes on localhost:' + port);
