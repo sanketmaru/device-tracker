99e3f4329c8ddf16aa08700f282cb009e3f71313
sanketmaru <sanketmaru@gmail.com>
2016-02-07 13:10:42 +0530

    server js changes

--- server.js
+++ server.js
@@ -1,25 +1,15 @@
-var app = require('http').createServer(handler)
-var io = require('socket.io')(app);
-var fs = require('fs');
+var app = require('express')();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+server.listen(process.env.PORT || 8082);
 
-app.listen(9000);
-
-function handler (req, res) {
-  fs.readFile('./public' + '/index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
-
-    res.writeHead(200);
-    res.end(data);
+io.sockets.on('connection', function (socket) {
+  socket.on('location', function (data) {
+    io.sockets.emit('location', data);
   });
-}
+});
 
-io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
+app.get('/', function(req, res) {
+  //send the index.html in our public directory
+  res.sendfile('./public/index.html');
 });
